9de93029a35990a4ec762fe100498eff2b06e228
Simon Boyer <si.boyer@hotmail.ca>
2022-06-21 19:27:49 -0400

    first test provision

---
+++ dietpi-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip3 install ansible
+
+ansible-pull--purge -d /var/tmp/ansible-homeserver -i /var/tmp/ansible-homeserver/ansible/inventory.yml \ 
+-U https://github.com/Simon-Boyer/Homeserver.git ansible/playbook.yml
