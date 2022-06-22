f0f472be0147056ade5ae4b0a6479b75f307ada1
Simon Boyer <si.boyer@hotmail.ca>
2022-06-21 20:06:09 -0400

    error in script

--- dietpi-script.sh
+++ dietpi-script.sh
@@ -2,5 +2,5 @@
 
 pip3 install ansible
 
-ansible-pull--purge -d /var/tmp/ansible-homeserver -i /var/tmp/ansible-homeserver/ansible/inventory.yml \ 
+ansible-pull --purge -d /var/tmp/ansible-homeserver -i /var/tmp/ansible-homeserver/ansible/inventory.yml \ 
 -U https://github.com/Simon-Boyer/Homeserver.git ansible/playbook.yml
