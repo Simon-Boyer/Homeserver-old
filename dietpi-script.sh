105e43f1a9df8a38fa665332ccfb65587b4b514f
Simon Boyer <si.boyer@hotmail.ca>
2022-06-21 21:44:39 -0400

    netcat

--- dietpi-script.sh
+++ dietpi-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+apt install netcat
 pip3 install ansible
 
 ansible-galaxy install xanmanning.k3s
-
 ansible-pull --purge -d /var/tmp/ansible-homeserver -i /var/tmp/ansible-homeserver/ansible/inventory.yml \
 -U https://github.com/Simon-Boyer/Homeserver.git ansible/playbook.yml
